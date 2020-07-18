d692904f3585b0ddd7d374d193cc9890a3804128
shubham giri <41017859+ishubs@users.noreply.github.com>
2020-07-18 11:10:06 +0530

    Updates

---
+++ precache-manifest.4f90380400cd4c5c7c4573053f226f30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2691892e2e6f4f37b1a0a58265238cb0",
+    "url": "/react-times/index.html"
+  },
+  {
+    "revision": "253a7d87e7c3cabce0bb",
+    "url": "/react-times/static/css/2.cd507353.chunk.css"
+  },
+  {
+    "revision": "8ee5db6a3925dd9ae7ff",
+    "url": "/react-times/static/css/main.9e6f27e2.chunk.css"
+  },
+  {
+    "revision": "253a7d87e7c3cabce0bb",
+    "url": "/react-times/static/js/2.8c7e8189.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-times/static/js/2.8c7e8189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee5db6a3925dd9ae7ff",
+    "url": "/react-times/static/js/main.6d1090d7.chunk.js"
+  },
+  {
+    "revision": "0b3fb8a76f53af2c5b86",
+    "url": "/react-times/static/js/runtime-main.90b9bb5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-times/static/media/logo.5d5d9eef.svg"
+  }
+]);
